dab57d97998995a1c654f668cdfeece18f1ddf79
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-10-21 14:14:55 +0200

    Add back tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+export const theme = {
+    extend: {
+        colors: {
+            // Primary UniGe colors
+            'unige-blue': '#002677',
+            'unige-yellow': '#F4DA40',
+            'unige-red': '#C8102E',
+            'unige-light-blue': '#199BFC',
+            'unige-dark': '#25282A',
+            'unige-gray': '#D9D9D6',
+
+            // Scientific areas
+            'giurisprudenza': '#0061A0',
+            'scienze-mfn': '#007C58',
+            'scienze-politiche': '#00A7B5',
+            'lingue-culture': '#A4BCC2',
+            'lettere-filosofia': '#B79A81',
+            'economia': '#F1C400',
+            'architettura-design': '#F4633A',
+            'medicina': '#EF3340',
+            'scienze-formazione': '#9B3259',
+            'ingegneria': '#574676',
+            'farmacia': '#E0A4A1',
+        },
+
+        fontFamily: {
+            sans: ['Fira Sans', 'sans-serif'],
+            serif: ['Roboto Slab', 'serif'],
+        },
+    },
+};
